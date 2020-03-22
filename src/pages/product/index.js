41d37e63cb937bbfbc3dc958e31fe9d41a044f3c
unknown <felipeAgomes>
2020-03-22 01:49:59 -0300

    Navegando pro detalhe

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,7 +1,32 @@
 import React, { Component } from "react";
+import api from '../../services/api';
+import './styles.css';
 
 export default class Product extends Component {
+    state = {
+        Product:{},
+    };
+
+
+    async componentDidMount(){
+
+        const { id } = this.props.match.params;
+        const response = await api.get(`/products/${id}`);
+
+        this.setState({Product: response.data});
+
+    }
     render(){
-        return <h1>Product</h1>
+
+        const { Product } = this.state;
+        return(
+            <div className="product-info">
+                <h1>{Product.title}</h1>
+                <p>{Product.description}</p>
+                <p>
+                  URL: <a href={Product.url}>{Product.url}</a>
+                </p>
+            </div>
+        );
     }
 };
